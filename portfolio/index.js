ba1e5256ae90fbfa04b206f83c7793c0524132cb
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-01-28 17:17:54 +0500

    feat: add buttons effects

--- portfolio/index.js
+++ portfolio/index.js
@@ -356,3 +356,23 @@ if (localStorage.theme === 'dark') {
     }
   });
 }
+
+// effect buttons
+
+const effectBtns = document.querySelectorAll('.button');
+effectBtns.forEach((el) =>
+  el.addEventListener('click', function (e) {
+    const x = e.pageX;
+    const y = e.pageY;
+    const buttonTop = e.target.offsetTop;
+    const buttonLeft = e.target.offsetLeft;
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+    const circle = document.createElement('span');
+    circle.classList.add('circle');
+    circle.style.top = yInside + 'px';
+    circle.style.left = xInside + 'px';
+    this.appendChild(circle);
+    setTimeout(() => circle.remove(), 500);
+  })
+);
